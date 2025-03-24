8b7d6ca0d06755f69477d60a3dd47c375fe6a94d
Rithikreddy <mandipat@usc.edu>
2025-03-24 12:09:22 -0700

    added transformation pipeline

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Datascience_Project
+
+
+### Workflow-Ml Pipeline
+
+1. Data ingestion
+2. Data Tranformation
+3. Model Trainer
+4. Model Evaluation
+
+## workflows
+
+1. Update config.yaml
+2. Update schema.yaml 
+3. Update params.yaml
+4. 
---
+++ main.py
@@ -0,0 +1,38 @@
+from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+
+
+STAGE_NAME="Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="Data Validation Stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.initiate_data_validation()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="Data Transformation Stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.initiate_data_transformation()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")    
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
